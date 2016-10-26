481607305e9852fda496a5b1908064107fc131c8
taiChantarungsee <tai@reportlab.com>
2016-10-26 12:53:05 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to twitter-bot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ profiles.clj
@@ -0,0 +1,4 @@
+{:dev  {:env {:app-consumer-key "7UvuV689aCS0Ddmv8QuT8x72X"
+                            :app-consumer-secret "v9Ep1nBsYXbNzoiJhSMjP2REcE4aV5K2WKWy8sYAzzJ0i6yrb4"
+                            :user-access-token "791222562571313152-nRHpmU3O23XkeoZFW7tEWFaWoJlG77v"
+                            :user-access-secret "sGSWRd3Ocg9WIZodIZv7fpJuJRr4dCqYCd1w4jBMPT5li"}}}
